b0602544c2efca88ea3989dd2f101098d89fc093
HarikaPatro <patroharika866@gmail.com>
2026-02-01 13:10:26 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "please enter user name"
+read USER_NAME   #HERE USER_NAME IS VARIABLE
+echo "please enter password"
+read -s PASSWORD
+
+echo "username is : $USER_NAME"
+echo "password is: $PASSWORD"
---
+++ 04-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+Current_Date=$(date)
+
+echo "current date is:$Current_Date"
