a1555c22ead2d03a9b9cd8748b565871baed28f4
HarikaPatro <patroharika866@gmail.com>
2026-02-04 20:01:18 +0530

    shell-practice

---
+++ 17-disk-usage.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DISK_USAGE=$( df -hT | grep -v Filesystem )
+DISK_THRESHOLD=2 #in projects we keep as 75
+IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+MESSAGE=""
+
+while IFS= read -r line
+do
+    USAGE=$( echo $line | awk '{print $6}' | cut -d "%" -f1 )
+    PARTITION=$( echo $line | awk '{print $7}' )
+    if [ $USAGE -ge $DISK_THRESHOLD ]; then
+        MESSAGE+="High Disk usage on $PARTITION: $USAGE % \n"
+    fi
+done <<<$DISK_USAGE
+
+echo -e "message body: $MESSAGE"
+
+sh mail.sh #pass the arguments
+
+#TO_ADDRESS=$1
+#SUBJECT=$2
+#ALERT_TYPE=$3
+#MESSAGE_BODY=$4
+#IP_ADDRESS=$5
+#TO_TEAM=$6
